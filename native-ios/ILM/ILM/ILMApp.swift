e151e5adb616d554e08a2b79c6514e4b111e128a
Mohamed Jamshed <mohamedjamsheddxb@gmail.com>
2026-08-23 08:49:51 -0700

    Add native iOS SwiftUI project foundation

---
+++ native-ios/ILM/ILM/ILMApp.swift
@@ -0,0 +1,17 @@
+//
+//  ILMApp.swift
+//  ILM
+//
+//  Created by Mohamed Jamshed on 23/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct ILMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
